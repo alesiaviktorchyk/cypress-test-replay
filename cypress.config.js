48cedff69ae4214f236dc80dbc37869a4d5c1664
Alesia Viktorchyk <alesia.viktorchyk@codex-soft.com>
2023-11-27 17:44:08 +0100

    chore: record videos

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,23 @@
 const { defineConfig } = require("cypress");
+const fs = require("fs");
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on('after:spec', (spec, results) => {
+        if (results && results.video) {
+          // Do we have failures for any retry attempts?
+          const failures = results.tests.some(test => test.attempts.some(attempt => attempt.state === 'failed'));
+          if (!failures) {
+            // delete the video if the spec passed and no tests retried
+            fs.unlinkSync(results.video);
+          }
+        }
+      });
     },
   },
   projectId: "6xx3qp",
   viewportWidth: 1920,
   viewportHeight: 1080,
+  video: true,
 });
